8496f0e753419f3e14d116f96f8be1cd09d949ba
MagicUmom <MCLAB@mclabde-mac-mini.local>
2016-10-12 22:19:31 +0800

    Initial Commit

---
+++ Traffic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Traffic
+//
+//  Created by MCLAB on 2016/10/12.
+//  Copyright © 2016年 MCLAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
