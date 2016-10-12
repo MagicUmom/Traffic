d2ced8c68565fc8aa79246d02e6aa705d9489d3a
MagicUmom <MCLAB@MCLABde-Mac-mini.local>
2016-10-12 23:48:33 +0800

    add png to map view

---
+++ Traffic/mapViewController.h
@@ -0,0 +1,13 @@
+//
+//  mapViewController.h
+//  Traffic
+//
+//  Created by MCLAB on 2016/10/12.
+//  Copyright © 2016年 MCLAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mapViewController : UIViewController
+
+@end
